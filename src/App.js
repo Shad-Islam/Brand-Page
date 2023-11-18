528fa82af30fe86afe8207c7fc59ab4040799709
Shad-islam <shadislam48@gmail.com>
2023-11-18 23:00:41 +0600

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <div className="icon"></div>
+        <div className="menu"></div>
+        <div className="search"></div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
