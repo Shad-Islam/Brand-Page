b1e0d814e1a72c439174bc1cee939a78e86a965e
Shad-islam <shadislam48@gmail.com>
2023-11-30 23:44:22 +0600

    Add products components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./components/nav";
+import Products from "./components/product";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Products />
     </div>
   );
 }
