1ed17658d89ac126f1062439990e6a09930ae9c2
Shad-islam <shadislam48@gmail.com>
2023-12-02 23:39:37 +0600

    product componenr remove from app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Nav from "./components/nav";
-import Products from "./components/product";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Products />
     </div>
   );
 }
