12af96810a4ea2d014ef2b8b3c1da9f3125f60f8
Shad-islam <shadislam48@gmail.com>
2023-12-03 23:20:27 +0600

    Delete nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import Nav from "./components/nav";
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/nav.jsx
+++
@@ -1,9 +0,0 @@
-function Nav() {
-  return (
-    <div>
-      <h1>Coming soon</h1>
-    </div>
-  );
-}
-
-export default Nav;
