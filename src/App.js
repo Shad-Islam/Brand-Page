ffc901e3f51e6599c041bb94b31148bc62c5fb01
Shad-islam <shadislam48@gmail.com>
2023-12-07 22:50:09 +0600

    Create a product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Product />
     </div>
   );
 }
