545651d3a7e5025cb01d40cd6dd8fc306e8e6f76
Shad-islam <shadislam48@gmail.com>
2023-12-08 23:20:12 +0600

    Use useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Product from "./components/Product";
+import { useEffect } from "react";
 
 function App() {
+  console.log("hello");
+
+  useEffect(() => {
+    console.log("effect");
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <div className="App">
       <Nav />
